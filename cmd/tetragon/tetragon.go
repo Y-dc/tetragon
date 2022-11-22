6ab015dcad4c4f89bcd1822caff3671b476f57f4
dechengyuan <dechengyuan@tencent.com>
2022-11-22 11:15:36 +0800

    feat: add pprof

--- cmd/tetragon/tetragon.go
+++ cmd/tetragon/tetragon.go
@@ -4,12 +4,33 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
+
+	"github.com/gin-contrib/pprof"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	go goPprof()
 	if err := execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
+}
+
+func goPprof() {
+	router := gin.New()
+	pprof.Register(router)
+	httpServer := &http.Server{
+		Addr:         ":80",
+		Handler:      router,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 2 * time.Minute,
+		IdleTimeout:  13 * time.Minute,
+	}
+
+	log.Println("开始服务")
+	log.Fatal(httpServer.ListenAndServe())
 }

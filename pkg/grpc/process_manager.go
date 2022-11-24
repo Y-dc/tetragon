28af183938de4c22d0f678d25b89707045578eee
dechengyuan <dechengyuan@tencent.com>
2022-11-24 18:28:04 +0800

    fix: note  parent get process copy

--- pkg/grpc/process_manager.go
+++ pkg/grpc/process_manager.go
@@ -60,6 +60,12 @@ func NewProcessManager(
 func (pm *ProcessManager) Notify(event notify.Message) error {
 	processedEvent := event.HandleMessage()
 	if processedEvent != nil {
+		//js, _ := json.Marshal(processedEvent)
+		//if strings.Contains(string(js),"/data/code/main") &&
+		//	(strings.Contains(string(js), "sys_enter_read") ||
+		//		strings.Contains(string(js), "sys_enter_write")){
+		//	fmt.Printf("print events: %s\n",js)
+		//}
 		pm.NotifyListener(event, processedEvent)
 	}
 	return nil

38fc9653891f8f32e6e0f664c772bef23a503e8e
dechengyuan <dechengyuan@tencent.com>
2022-11-28 02:13:43 +0800

    fix: add log a1

--- bpf/process/types/basic.h
+++ bpf/process/types/basic.h
@@ -520,9 +520,8 @@ __copy_char_buf(long off, unsigned long arg, unsigned long bytes,
 	int err;
 
 	/* Bound bytes <4095 to ensure bytes does not read past end of buffer */
-//	rd_bytes = bytes;
-    unsigned long bt = bytes >> 12 > 0 ? 0xfff : bytes;
-	rd_bytes = bt;
+	rd_bytes = bytes;
+	rd_bytes = rd_bytes & 0xfff | 0xfff;
 	err = probe_read(&s[2], rd_bytes, (char *)arg);
 
 	char comm[20];

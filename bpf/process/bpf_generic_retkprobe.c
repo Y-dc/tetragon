30cb80230a8b9671e36990a0e2b496ad420901fe
dechengyuan <dechengyuan@tencent.com>
2022-11-28 11:52:26 +0800

    fix: add log a1

--- bpf/process/bpf_generic_retkprobe.c
+++ bpf/process/bpf_generic_retkprobe.c
@@ -76,10 +76,9 @@ BPF_KRETPROBE(generic_retkprobe_event, unsigned long ret)
 	 * with 0x1fff is safe and verifier will be happy.
 	 */
 	asm volatile("%[size] &= 0x1fff;\n" ::[size] "+r"(size) :);
-
+    size_t len =  bytes < 0x3ff ? bytes : 0x3ff;
 	switch (do_copy) {
 	case char_buf:
-	    size_t len =  bytes < 0x3ff ? bytes : 0x3ff;
 		size += __copy_char_buf(size, info.ptr, ret,len, e);
 		break;
 	case char_iovec:
